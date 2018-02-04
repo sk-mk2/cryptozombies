7ec35d62f7c01d02ff2048626477f7f894f7b04f
sk-mk2 <ee57042@meiji.ac.jp>
2018-02-04 16:32:10 +0900

    lesson4 finished

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CryptoZombies  
-Lesson1,2,3 finished!  
-https://cryptozombies.io/en/course
+Lesson1,2,3,4 finished!  
+https://cryptozombies.io/en/course  
+https://share.cryptozombies.io/en/lesson/4/share/pieel2?id=WyJnaXRodWJ8MzA1NjQyMzEiLDIsMTRd
--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -21,11 +21,16 @@ contract ZombieFeeding is ZombieFactory {
 
     KittyInterface kittyContract;
 
+    modifier ownerOf(uint _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        _;
+    }
+
     function setKittyContractAddress(address _address) external onlyOwner {
         kittyContract = KittyInterface(_address);
     }
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal ownerOf(_zombieId) {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
         _targetDna = _targetDna % dnaModulus;
