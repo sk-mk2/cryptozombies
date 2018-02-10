c8526ae1b2ef7ce047c8220ede625846e6484404
sk-mk2 <ee57042@meiji.ac.jp>
2018-02-10 18:21:09 +0900

    geth tutorial finished

---
+++ eth_private_net/SingleNumRegister.sol
@@ -0,0 +1,10 @@
+contract SingleNumRegister {
+    uint storedData;
+    function set(uint x) {
+        storedData = x;
+    }
+    function get() constant returns (uint retVal) {
+        return storedData;
+    }
+}
+
